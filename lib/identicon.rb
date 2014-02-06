f99fb3ec12d31c435972eeb2645ff1445a46a713
Max Power <kevin.melchert@gmail.com>
2014-02-06 11:07:19 +0100

    foreground & background color to icon

--- lib/identicon.rb
+++ lib/identicon.rb
@@ -3,11 +3,11 @@ require "digest/sha1"
 require "matrix"
 
 class Identicon
-  def initialize(data='', cols: 5, rows: 5)
+  def initialize(data='', cols: 5, rows: cols)
     @data = data.to_s
     @rows = rows.to_i
     @cols = cols.to_i
-    @half = (cols.to_f / 2).ceil
+    @half = (@cols + 1) / 2
   end
   
   def digest
@@ -17,20 +17,30 @@ class Identicon
   def binary
     digest.to_i(16).to_s(2).split('').map(&:to_i)
   end
+
+  def foreground_color
+    "#" + digest[0,6]
+  end
+  
+  def background_color
+    "#e0e0e0"
+  end
+  
+  def color(foreground=true)
+    foreground ? foreground_color : background_color
+  end
   
   def matrix
     Matrix.columns prepare
   end
   
-  def color
-    "#" + digest[0,6]
-  end
-  
   private
   
   def prepare
-    binary.reverse.each_slice(@rows).take(@half).tap do |a|
-      @half.upto(@cols-1) { |i| a[i] = a[@cols-i-1] }
+    binary.reverse.each_slice(@rows).take(@half).tap do |matrix|
+      (@half...@cols).each do |row|
+        matrix[row] = matrix[@cols-1-row]
+      end
     end
   end
 end
